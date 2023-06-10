5c1235688030d476ff81aff92a1ff6596f3280e4
olaniyanp11 <olaniyanp11@gmail.com>
2023-06-10 15:40:14 +0000

    successfully created the db stirage file

--- blog/models.py
+++ blog/models.py
@@ -19,7 +19,7 @@ class Post(db.Model):
       title = db.Column(db.String(100),nullable=False)
       date = db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
       content = db.Column(db.Text, nullable=False)
-      user_id= db.Column(db.Integer, db.ForeignKey('user_id'),nullable =False)
+      user_id= db.Column(db.Integer, db.ForeignKey('user.id'),nullable =False)
 
       def __repr__(self):
         return f"Post('{self.title}', '{self.date}')"
